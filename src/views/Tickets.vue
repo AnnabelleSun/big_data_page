da6a7405d4e388ca22a06e0a4719e7343386bd16
AnnabelleSun <3190218383@qq.com>
2021-04-21 20:12:50 +0800

    feat: tickets;

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div class="tickets">
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+      <el-col :span="12">
+        <div class="container"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss">
+.tickets {
+  height: 100%;
+  .el-row {
+    height: 100%;
+  }
+  .el-col {
+    height: 100%;
+    .container {
+      height: 100%;
+    }
+  }
+}
 </style>
