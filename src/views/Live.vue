f6245d9493ff8dc24f664b3f180efe862dddb764
AnnabelleSun <3190218383@qq.com>
2021-04-21 20:05:32 +0800

    style: full height;

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -1,28 +1,37 @@
 <template>
-  <div>
+  <div class="live">
     <el-row :gutter="20">
-  <el-col :span="6">
-    <div class="container"></div>
-  </el-col>
-  <el-col :span="6">
-    <div class="container"></div>
-  </el-col>
-  <el-col :span="6">
-    <div class="container"></div>
-  </el-col>
-  <el-col :span="6">
-    <div class="container"></div>
-  </el-col>
-</el-row>
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="container"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang="scss">
+.live {
+  height: 100%;
+  .el-row {
+    height: 100%;
+  }
+  .el-col {
+    height: 100%;
+    .container {
+      height: 100%;
+    }
+  }
+}
 </style>
