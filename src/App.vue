537867921c97a5d8a1c53324d20acb27314262ad
AnnabelleSun <3190218383@qq.com>
2021-04-21 17:46:05 +0800

    feat: startingl

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <div id="nav">
+    <div>{{title}}</div>
+    <router-link v-for="item in routerLinks" :key="item.id" :to="item.to">{{item.text}}</router-link>
   </div>
   <router-view />
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      title: '大数据分析系统',
+      routerLinks: [
+        {
+          id: 0,
+          to: '/live',
+          text: '实时监测'
+        },
+        {
+          id: 1,
+          to: '/tickets',
+          text: '票务信息'
+        },
+        {
+          id: 2,
+          to: '/market',
+          text: '营销数据'
+        },
+        {
+          id: 3,
+          to: '/operate',
+          text: '运营数据'
+        },
+        {
+          id: 4,
+          to: '/tourist',
+          text: '用户画像'
+        },
+        {
+          id: 5,
+          to: '/emergency',
+          text: '应急事件'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+@import url(./assets/style/reset.css);
+#app {
+  width: 100%;
+  height: 100%;
+}
+.app {
+  background: url(./assets/img/bg.jpeg);
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Live from '../views/Live.vue'
+import Market from '../views/Market.vue'
+import Tickets from '../views/Tickets.vue'
+import Operate from '../views/Operate.vue'
+import Tourist from '../views/Tourist.vue'
+import Emergency from '../views/Emergency.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    to: '/live',
+    name: '实时监测',
+    component: Live
+  },
+  {
+    to: '/tickets',
+    name: '票务信息',
+    component: Tickets
+  },
+  {
+    to: '/market',
+    name: '营销数据',
+    component: Market
+  },
+  {
+    to: '/operate',
+    name: '运营数据',
+    component: Operate
+  },
+  {
+    to: '/tourist',
+    name: '用户画像',
+    component: Tourist
+  },
+  {
+    to: '/emergency',
+    name: '应急事件',
+    component: Emergency
   },
   {
     path: '/about',
