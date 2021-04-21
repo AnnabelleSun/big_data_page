a4bd88330a8bbd9f28f76b8e8e61a76236183911
meinvzi <1289104698@qq.com>
2021-04-21 16:52:13 +0800

    feat: fw's first push;

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import * as echarts from 'echarts';
+import scyEcharts from 'scyecharts';
+Vue.prototype.$echarts = echarts; // 引入echarts
+Vue.use(scyEcharts);
 
 createApp(App).use(router).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,124 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="box">
+    <div class="ulbox">
+      <ul>
+        <li>实时监测</li>
+        <li>票务信息</li>
+        <li>营销数据</li>
+        <li class="l">大数据分析系统</li>
+        <li>运营数据</li>
+        <li>游客画像</li>
+        <li>应急事件</li>
+      </ul>
+    </div>
+    <div class="selbox">
+      <p>请选择年份：</p>
+      <select >
+        <option>广州</option>
+        <option>深圳</option>
+        <option>山东</option>
+      </select>
+      <p>请选择年份：</p>
+      <select >
+        <option>广州</option>
+        <option>深圳</option>
+        <option>山东</option>
+      </select>
+      <p>请选择年份：</p>
+      <select >
+        <option>广州</option>
+        <option>深圳</option>
+        <option>山东</option>
+      </select>
+      <button>确认</button>
+    </div>
+    <div class="lebox">
+      <p>dd</p>
+      <p id="myChart" style="width: 600px;height:400px;">dhhjhjhd</p>
+
+    </div>
+    <div class="cebox">
+      <p>dd</p>
+    </div>
+    <div class="ribox">
+      <p>dd</p>
+    </div>
   </div>
 </template>
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+ 
+})
+</script>
+
+
+
+<style scoped>
+.box{
+  background: rgb(61, 61, 243);
+  width: 100%;
+  height: 800px;
+  display: block;
+}
+.ulbox>ul>li{
+  list-style: none;
+  display: inline;
+  font-size: 18px;
+  padding: 20px 40px;
+  text-align: center;
+  line-height: 80px;
+  color: white;
+}
+.ulbox>ul>.l{
+font-size: 40px;
+padding: 10px 40px;
+}
+.selbox>p{
+  display: inline;
+  color: white;
+  padding-left: 30px;
+}
+.selbox>select{
+  width: 100px;
+  height: 25px;
+  border-color: white;
+  color: rgb(67, 190, 238);
+  font-size: 13px;
+  border: 1px solid rgb(67, 190, 238);
+  outline:none;
+}
+.selbox>button{
+  color: white;
+  font-size: 12px;
+  background-color: rgb(67, 190, 238);
+  border: none;
+  padding: 5px 10px;
+  border-radius: 3px;
+  margin-left: 20px;
+  outline:none;
+}
+.lebox{
+  float: left;
+  width: 40%;
+}
+.lebox>p{
+  width: 20%;
+  height: 10%;
+  border: 1px solid red;
+}
+.cebox{
+  float: left;
+  width: 30%;
+}
+.ribox{
+  float: right;
+  width: 30%;
+}
+</style>
