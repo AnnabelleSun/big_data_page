648ce74fa13ee7c095c4519d011038c1ab26a850
wb_sunrurui <wb_sunrurui@meituan.com>
2021-05-17 16:59:48 +0800

    feat: table;

--- src/views/Market.vue
+++ src/views/Market.vue
@@ -7,6 +7,7 @@
     :data="tableData"
     tooltip-effect="dark"
     style="width: 100%"
+    v-loading="loading"
     @selection-change="handleSelectionChange">
     <el-table-column
       type="selection"
@@ -31,8 +32,22 @@
   <div style="margin-top: 20px">
     <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
     <el-button @click="toggleSelection()">取消选择</el-button>
-    <el-button @click="addSelection()">添加</el-button>
+    <!-- <el-button @click="addSelection()">添加</el-button> -->
+    <el-button @click="dialogVisible = true">点击打开 Dialog</el-button>
   </div>
+
+  <el-dialog
+    title="提示"
+    v-model="dialogVisible"
+    width="30%">
+    <span>这是一段信息</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogAction(true)">确 定</el-button>
+      </span>
+    </template>
+  </el-dialog>
 
   </div>
 </template>
@@ -41,6 +56,8 @@
   export default {
     data() {
       return {
+        loading: false,
+        dialogVisible: false,
         tableData: [{
           date: '2016-05-03',
           name: '王小虎',
@@ -89,6 +106,7 @@
       },
       addSelection () {
         console.log('[addSelection]')
+        this.loading = true
         const _data = [...this.tableData, {
           date: '2021-05-17',
           name: '苏森林',
@@ -96,7 +114,15 @@
         }]
         setTimeout(() => {
           this.tableData = _data
+          this.loading = false
         }, 3000)
+      },
+      dialogAction (flag) {
+        console.log('[dialogAction]', flag)
+        if (flag) {
+          this.dialogVisible = false
+          this.addSelection()
+        }
       }
     }
   }
